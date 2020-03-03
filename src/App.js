18ade67b9d9cf3a01e2483623e5843d47b5dcc12
Janne <peterdev80@gmail.com>
2020-03-03 11:07:06 +0200

    smooth scroll

--- src/App.js
+++ src/App.js
@@ -7,23 +7,24 @@ import './css/app.css'
 import Footer from './components/Footer';
 import Desserts from './components/Desserts';
 import landingImg from './images/dish.jpg'
-function App() { 
-  const [page,setPage]=useState('contact')
-  const changePage =(topage)=>{
+function App() {
+  const [page, setPage] = useState('contact')
+  const changePage = (topage) => {
+    document.getElementById('infocontainer').scrollIntoView({ behavior: 'smooth' })
     setPage(topage)
   }
   return (
-    <div>
-      <div  className='page'>
+    <div className='page'>
       <Menubar changePage={changePage} ></Menubar>
       <div className='header'></div>
       <img className='landingimage' src={landingImg} alt='not found'></img>
-      <Contact show={page==='contact'}></Contact>
-      <Meny show={page==='menu'}></Meny>
-      <Drinks show={page==='drinks'}></Drinks>
-      <Desserts show={page==='desserts'}></Desserts>
-      <Footer></Footer>
+      <div id='infocontainer' className='appinfocontainer'>
+        <Contact show={page === 'contact'}></Contact>
+        <Meny show={page === 'menu'}></Meny>
+        <Drinks show={page === 'drinks'}></Drinks>
+        <Desserts show={page === 'desserts'}></Desserts>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
